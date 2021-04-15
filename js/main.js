ecc29af2fedf79f0a36d1605f0d123af77abea4e
webdesignmind <axenoffdenis@yandex.ru>
2021-04-15 23:15:38 +0300

    change: added slider + offer sections

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const swiper = new Swiper(".slider-container", {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 1,
+
+  // If we need pagination
+  pagination: {
+    el: ".slider-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".slider-button-next",
+    prevEl: ".slider-button-prev",
+  },
+
+  effect: "slide",
+  keyboard: {
+    enabled: true,
+  },
+});
